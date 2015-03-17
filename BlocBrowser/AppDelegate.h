e4a5b58ea82d0fc8beb55cbd42cc870dba0f2b02
edheywood <edward@waffleapp.co>
2015-03-17 14:52:29 +0000

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Edward Heywood on 17/03/2015.
+//  Copyright (c) 2015 Edward Heywood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Edward Heywood on 17/03/2015.
+//  Copyright (c) 2015 Edward Heywood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
