977e8fbf25cd3a3f008faca35034951547c9ba4f
edheywood <edward@waffleapp.co>
2015-03-18 23:51:43 +0000

    Functional web browser

---
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Edward Heywood on 17/03/2015.
+//  Copyright (c) 2015 Edward Heywood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
